64a59db09263320474b37f6e5fea0ea0686bb83f
alice-yang94 <aliceyang94@hotmail.com>
2019-11-12 20:14:03 +1100

    Completed server timeout logout and client actively logout

--- assignment/client.py
+++ assignment/client.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 #Usage: python3 client.py server_addr server_port
 import socket
+import select
 import sys
 
+# handle the data received from server
 def data_handler(data):
+    global curr_state
     status, msg = data.split('\n',1)
     status = status.split()
     if msg:
@@ -16,12 +19,18 @@ def data_handler(data):
         return ' '.join([status[0], status[1], response])
 
     if status[0] == 'OK':
-        if status[1] == 'login':
+        if status[1] == 'login' or status[1] == 'logout':
+            curr_state = status[1]
             return ''
         else:
             # implement other status types
-            return ''    
-    
+            return ''
+
+    # received timeout msg from server due to inactivity
+    if status[0] == 'TIMEOUT':
+        curr_state = 'logout'
+        return ''
+
     # status[0] == 'ERROR'
     if status[1] == 'login':
         return ''
@@ -29,23 +38,46 @@ def data_handler(data):
         # implement other status types
         return ''
 
+def main():
+    # Read server name and port number from command line
+    serverName = sys.argv[1]
+    serverPort = int(sys.argv[2])
+    server_address = (serverName, serverPort)
 
-# Read server name and port number from command line
-serverName = sys.argv[1]
-serverPort = int(sys.argv[2])
-server_address = (serverName, serverPort)
+    # create tcp socket
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect to server
+    clientSocket.connect(server_address)
+    # set clientSocket to non-blocking
+    clientSocket.setblocking(False)
+    
+    # curr_state: authen/login/logout
+    global curr_state
+    curr_state = 'authen'
+    
+    while curr_state != 'logout':
+        # since clientSocket is non-blocking, use try to recv data, if 
+        #nothing received, continue
+        try:
+            data = clientSocket.recv(2048).decode()
+            if data:
+                response = data_handler(data).encode()
+                if curr_state != 'logout' and response:
+                    clientSocket.sendto(response, server_address)
+        except:
+            pass
 
-# create tcp socket
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # if login and there is input, send it to server, else continue
+        if curr_state == 'login' \
+            and sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            request = sys.stdin.readline()
+            if request:
+                request = request.encode()
+                clientSocket.sendto(request, server_address)
 
-# connect to server
-clientSocket.connect(server_address)
-logout = False
-while not logout:
-    data = clientSocket.recv(4096).decode()
-    if data:
-        response = data_handler(data).encode()
-        clientSocket.sendto(response, server_address)
-
-clientSocket.close()
-# Close the socket
+    # Close the socket
+    clientSocket.close()
+    
+           
+if __name__ == "__main__":
+    main()
--- assignment/server.py
+++ assignment/server.py
@@ -4,51 +4,59 @@
 import sys      
 import socket
 from datetime import datetime
+from collections import defaultdict
 
-def login(client_addr):
-    clients[client_addr]['status'] = 'login'
-    clients[client_addr]['login_time'] = datetime.now()
+
+def login(usern):
+    clients[usern]['status'] = 'login'
+    clients[usern]['login_time'] = datetime.now()
+    clients[usern]['last_activate_time'] = datetime.now()
     # implement broadcast login here...
 
-def authen_usern(usern, client_addr):
-    if usern in credentials.keys():
-        clients[client_addr]['username'] = usern
+def logout(usern, client_addr, is_timeout):
+    clients[usern]['status'] = 'logout'
+    addr_to_user[client_addr] = None
+    if is_timeout:
+        status = 'TIMEOUT logout\n'
+        msg = 'Timeout. You are logged out due to inactivity'
+        return status + msg
+    # user actively logout
+    status = 'OK logout\n'
+    return status
+
+def authen_usern(input_usern, client_addr):
+    if input_usern in credentials.keys():
+        addr_to_user[client_addr] = input_usern
+        clients[input_usern]['status'] = 'authen'
         return 'authenticate Password\n'
-    # rsp: response to the authenticate request
-    # line1 - status, line2 - message
-    rsp = 'authenticate Username\n'
-    rsp += 'Invalid Username. Please try again'
-    return rsp
 
-def authen_passw(passw, client_addr):
-    usern = clients[client_addr]['username']
-    # rsp: response to the authenticate request
-    # line1 - status, line2 - message
+    # response: line1 - responseStatus, line2 - message
+    status = 'authenticate Username\n'
+    msg = 'Invalid Username. Please try again'
+    return status + msg
+
+def authen_passw(passw, usern):
     if credentials[usern] == passw:
-        login(client_addr)
-        rsp = 'OK login\n'
-        rsp += 'Welcome to the greatest messaging application ever!'
-        return rsp
+        login(usern)
+        status = 'OK login\n'
+        msg = 'Welcome to the greatest messaging application ever!'
+        return status + msg
     
-    if 'count' in clients[client_addr].keys():
-        clients[client_addr]['count'] += 1
-        if clients[client_addr]['count'] == 3:
-            rsp = 'ERROR login\n'
-            rsp += 'Invalid Password. Your account has been blocked. ' \
-                 + 'Please try again later'
-            return rsp
+    if 'count' in clients[usern].keys():
+        clients[usern]['count'] += 1
+        if clients[usern]['count'] == 3:
+            status = 'ERROR login\n'
+            msg = 'Invalid Password. Your account has been blocked. ' \
+                + 'Please try again later'
+            return status + msg
     else:
-        clients[client_addr]['count'] = 1
+        clients[usern]['count'] = 1
 
-    rsp = 'authenticate Password\n'
-    rsp += 'Invalid Password. Please try again'
-    return rsp
-
+    status = 'authenticate Password\n'
+    msg = 'Invalid Password. Please try again'
+    return status + msg
 
 def display_online_users():
-    return ''
-
-def logout():
     return ''
 
 def broadcast():
@@ -68,14 +76,13 @@ def send_msg(tokens):
     #message = str(tokens[2])
     return ''
 
-
-def request_handler(request, client_addr):
+def get_response(request, usern, client_addr):
     tokens = request.split()
     if len(tokens) == 1:
         if tokens[0] == 'whoelse':
             return display_online_users()
         elif tokens[0] == 'logout':
-            return logout()
+            return logout(usern, client_addr, False)
 
     elif len(tokens) == 2:
         if tokens[0] == 'broadcast':
@@ -94,31 +101,85 @@ def request_handler(request, client_addr):
             if tokens[1] == 'Username':
                 return authen_usern(tokens[2], client_addr)
             elif tokens[1] == 'Password':
-                return authen_passw(tokens[2], client_addr)
+                return authen_passw(tokens[2], usern)
 
     return 'Error. Invalid command\n'
 
+def request_handler(client_socket, client_addr):
+    response = ''
+    logout_flag = False
+    if client_addr not in addr_to_user.keys():
+        # new connection setup
+        addr_to_user[client_addr] = ''
+        # ask for username
+        response += 'authenticate Username\n'
+    
+    else:
+        usern = addr_to_user[client_addr]
+
+        # if the client has username and its status is login
+        if usern and clients[usern]['status'] == 'login' and \
+            ((datetime.now() - clients[usern]['last_activate_time']).seconds) >= timeout:
+
+            # For debug:
+            #print('last_activate_time: ', clients[usern]['last_activate_time'])
+            #print('now: ', datetime.now())
+            #print('timeout: {}, diff: {}'.format(timeout, (datetime.now() - clients[usern]['last_activate_time']).seconds))
+
+            logout_flag = True
+            response += logout(usern, client_addr, True)
+        else:
+            try:
+                # Get the request data
+                request = client_socket.recv(2048).decode()
+                if request:     
+                    #For debug:
+                    print('request: ', request)
+                    
+                    if request == 'logout':
+                        logout_flag = True
+                        response += logout(usern, client_addr, False)
+                    else:
+                        if usern and clients[usern]['status'] == 'login':
+                            clients[usern]['last_activate_time'] = datetime.now()
+                        response += get_response(request, usern, client_addr)
+            except:
+                # if no request catched, continue
+                pass
+
+    if response:
+        # For debug:
+        #print('sending: ', response)
+
+        # Send the responseStatus and message back to the client
+        client_socket.send(response.encode())
+
+    return logout_flag
 
 def main():
-    # Store clients info in this list
-    # clients = {(client_addr, client_port): dict}
-    # dict = {'usern':_, login_time':_, 'status':_, 'timeout_remain':_,...}
-    global clients
-    clients = {}
+    global clients, addr_to_user, online_users
+    # Store clients info in this dictionary
+    # clients = {username: user_info_dict}
+    user_info_keys = ['client_addr', 'login_time', 'status', 
+                      'last_activate_time', 'block_time']
+    clients = defaultdict(lambda: dict.fromkeys(user_info_keys))
+    # addr_to_user = {client_addr: username}
+    addr_to_user = {}
+    # online_users = [username]
+    online_users = []
     
     # Create a TCP socket object(streaming)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
     
     # read server_port, block_duration and timeout from command line
     server_port = int(sys.argv[1])
-    server_addr = 'localhost'
-    s.bind((server_addr, server_port)) # Bind localhost and port with our socket
+    s.bind(('localhost', server_port)) # Bind localhost and port with our socket
 
-    #block_duration = int(sys.argv[2])
-    #timeout = int(sys.argv[3])
+    global block_duration, timeout, credentials
+    block_duration = int(sys.argv[2])
+    timeout = int(sys.argv[3])
 
     # load credentials into a dictionary
-    global credentials
     credentials = {}
     with open('credentials.txt','r') as cred_file:
         for line in cred_file:
@@ -130,35 +191,19 @@ def main():
     
     while True:
         # Establish connection with client.
-        client_socket, client_addr = s.accept()    
-
+        client_socket, client_addr = s.accept()
+        
+        client_socket.setblocking(False)
         # For debug: 
         print('Got connection from', client_addr)
 
-        while True:
-            response = ''
-            if client_addr not in clients.keys():
-                # new connection setup
-                clients[client_addr] = {}
-                # ask for username
-                response += 'authenticate Username\n'
-            else:
-                # Get the request data
-                request = client_socket.recv(4096).decode()
-                response += request_handler(request, client_addr)
-
-                # For debug: 
-                print('request: ', request)
-
-            print('sending...')
-        
-            # Send the status and message back to the client
-            client_socket.send(response.encode())
+        logout_flag = False
+        while not logout_flag:
+            # return False if logout (either from user msg or timeout)
+            logout_flag = request_handler(client_socket, client_addr)
 
         # Close the connection
         client_socket.close()                
-
-           
            
 if __name__ == "__main__":
     main()
